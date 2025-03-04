1659a9ade6fc11d7de1884ce5144d0fb246acf1b
tracer-pa-public <tracer@njuics.org>
2025-03-04 19:25:26 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user stocks > uname Linux debian-10-pa 4.19.0-27-686-pae #1 SMP Debian 4.19.316-1 (2024-06-25) i686 GNU/Linux > uptime 19:25:26 up 2 min,  1 user,  load average: 0.12, 0.09, 0.03 > unitime > 2b8624fb9c69b6e08347b65c97cb98b9bf262030

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -83,10 +83,30 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sub(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+	// printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
+	// fflush(stdout);
+	// assert(0);
+	uint32_t mask = 0;
+	switch(data_size)
+	{
+		case 8:mask = 0xff;break;
+		case 16:mask = 0xffff;break;
+		case 32:mask = 0xffffffff;break;
+		default:break;
+	}
+	src &= mask;
+	dest &= mask;
+
+	uint32_t res = dest - src;
+	res &= mask;
+	
+	cpu.eflags.CF = (src > dest);
+	cpu.eflags.OF = ((dest >> (data_size-1)) != (src >> (data_size -1))) && ((res >> (data_size -1)) != (dest >> (data_size -1)));
+	cpu.eflags.ZF = cal_zf(res);
+	cpu.eflags.SF = (res >> (data_size - 1) == 0x01);
+	cpu.eflags.PF = cal_pf(res);	
+	
+	return res;
 #endif
 }
 

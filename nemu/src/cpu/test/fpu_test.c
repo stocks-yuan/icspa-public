fa4eba83829ec758225794756c250be2299bccf3
tracer-pa-public <tracer@njuics.org>
2025-03-08 10:44:20 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user stocks > uname Linux debian-10-pa 4.19.0-27-686-pae #1 SMP Debian 4.19.316-1 (2024-06-25) i686 GNU/Linux > uptime 10:44:20 up 12:14,  1 user,  load average: 0.09, 0.09, 0.02 > unitime > 1dad9017b44169f88c42cd63c5f8fceb700e44c8

--- nemu/src/cpu/test/fpu_test.c
+++ nemu/src/cpu/test/fpu_test.c
@@ -23,7 +23,7 @@ void fpu_test_add()
 			b.fval = input[j];
 			res.fval = a.fval + b.fval;
 			res_fpu.val = internal_float_add(b.val, a.val);
-			//printf("float add a = %f, b = %f, ua = %x, ub = %x, res = %x, res_fpu = %x, res = %f, res_fpu = %f\n", a.fval, b.fval, a.val, b.val, res.val, res_fpu.val, res.fval, res_fpu.fval);
+			printf("float add a = %f, b = %f, ua = %x, ub = %x, res = %x, res_fpu = %x, res = %f, res_fpu = %f\n", a.fval, b.fval, a.val, b.val, res.val, res_fpu.val, res.fval, res_fpu.fval);
 			fflush(stdout);
 			assert(res_fpu.val == res.val);
 		}
@@ -183,10 +183,10 @@ void init_fpu_test()
 	n_zero.fraction = 0;
 	p_inf.sign = 0;
 	p_inf.exponent = 255;
-	p_zero.fraction = 0;
+	p_inf.fraction = 0;
 	n_inf.sign = 1;
 	n_inf.exponent = 255;
-	n_zero.fraction = 0;
+	n_inf.fraction = 0;
 	p_nan.sign = 0;
 	p_nan.exponent = 255;
 	p_nan.fraction = 0x400000;

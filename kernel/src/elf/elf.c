53b5e849ce6bbff57900df09cdde7b54f8436d1f
tracer-pa-public <tracer@njuics.org>
2025-03-13 11:45:55 +0800

    > clean > id icspa_mooc_student > user stocks > uname Linux debian-10-pa 4.19.0-27-686-pae #1 SMP Debian 4.19.316-1 (2024-06-25) i686 GNU/Linux > uptime 11:45:55 up  8:57,  1 user,  load average: 0.02, 0.03, 0.00 > unitime > 64a87007133251bb2a343db9567e817f39519814

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -41,8 +41,8 @@ uint32_t loader()
 			//panic("Please implement the loader");
 
 /* TODO: copy the segment from the ELF file to its proper memory area */
-			memcpy(ph->p_vaddr, ph->p_offset, ph->p_filesz);
-			memset(ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+			memcpy((void *)ph->p_vaddr, (void *)ph->p_offset,(size_t) ph->p_filesz);
+			memset((void *)(ph->p_vaddr + ph->p_filesz), 0, (size_t)(ph->p_memsz - ph->p_filesz));
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
 
 #ifdef IA32_PAGE

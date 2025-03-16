77d6152879d8848a5272c8246141cab85a118a82
tracer-pa-public <tracer@njuics.org>
2025-03-16 17:54:46 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user stocks > uname Linux debian-10-pa 4.19.0-27-686-pae #1 SMP Debian 4.19.316-1 (2024-06-25) i686 GNU/Linux > uptime 17:54:45 up 1 day,  2:50,  1 user,  load average: 0.07, 0.03, 0.01 > unitime > 4a0929ba3af4c533a637d96184102053eadf6c8e

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -8,8 +8,8 @@ void init_cache()
 	// implement me in PA 3-1
 	for (int i = 0; i < CACHE_LINE_NUM; i++) {
 		cache[i].vaild_bit = false;
-#cache[i].tag = 0;
-#memset(cache[i].data, 0, CACHE_BLOCK_SIZE);
+//cache[i].tag = 0;
+//memset(cache[i].data, 0, CACHE_BLOCK_SIZE);
 	}
 }
 

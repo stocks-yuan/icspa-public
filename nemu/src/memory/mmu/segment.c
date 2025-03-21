d24a5c759e618b7f2f44a56cd3efeffc86962bfb
tracer-pa-public <tracer@njuics.org>
2025-03-21 15:18:26 +0800

    > clean > id icspa_mooc_student > user stocks > uname Linux debian-10-pa 4.19.0-27-686-pae #1 SMP Debian 4.19.316-1 (2024-06-25) i686 GNU/Linux > uptime 15:18:26 up 2 days, 16:06,  1 user,  load average: 0.01, 0.02, 0.00 > unitime > ccba0c8357a03c0cc7cb1d7712cf2ec2aa9afa3

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -16,13 +16,13 @@ void load_sreg(uint8_t sreg)
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
-		SegDesc *segDesc = (void *)(hw_mem + cpu.gdtr.base + cpu.segReg[sreg].index * 8);
+	SegDesc *segDesc = (void *)(hw_mem + cpu.gdtr.base + cpu.segReg[sreg].index * 8);
 	cpu.segReg[sreg].base = segDesc->base_15_0 + (segDesc->base_23_16 << 16) + (segDesc->base_31_24 << 24);
 	cpu.segReg[sreg].limit = segDesc->limit_15_0 + (segDesc->limit_19_16 << 16);
 	cpu.segReg[sreg].privilege_level = segDesc->privilege_level;
 
 	assert(cpu.segReg[sreg].base == 0);
 	assert(cpu.segReg[sreg].limit == 0xFFFFF);
-	assert(segdesc -> granularity == 1);
-	assert(segdesc -> present == 1);
+	assert(segDesc -> granularity == 1);
+	assert(segDesc -> present == 1);
 }
